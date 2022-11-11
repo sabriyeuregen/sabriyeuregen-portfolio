3fff570c26e8154842baf4e715b85b3b12c493ac
sabriyeuregen <sabriyeuregen@gmail.com>
2022-11-11 22:53:09 +0300

    Optmized for mobile devices

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,41 +1,94 @@
-import "./Sidebar.scss"
-import { Link, NavLink } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEnvelope, faHome,  faSuitcase,  faUser} from "@fortawesome/free-solid-svg-icons"
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import './Sidebar.scss'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faClose,
+  faBars,
+  faHome,
+  faSuitcase,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
 const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
   return (
     <div className="nav-bar">
-     <Link className="logo" to="/"> {/* logo gelicek*/ }
-        <img className="sub-logo" src="" alt="logo"></img> 
-     </Link>
-     <nav>
-        <NavLink activeclassname="active" to="/home">
-           <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+      <Link className="logo" to="/">
+        {' '}
+        {/* logo gelicek*/}
+        <img className="sub-logo" src="" alt="logo"></img>
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          activeclassname="active"
+          to="/home"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
         </NavLink>
-        <NavLink activeclassname="active" className="about-link" to="/about">
-           <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
         </NavLink>
-        <NavLink activeclassname="active" className="contact-link" to="/contact">
-           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
         </NavLink>
-        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
-           <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e"></FontAwesomeIcon>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e"></FontAwesomeIcon>
         </NavLink>
-     </nav>
-     <ul>
-       <li>
-           <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sabriyeuregen/">
-            <FontAwesomeIcon icon={faLinkedin} ></FontAwesomeIcon>
-           </a>
-       </li>
-       <li>
-           <a target="_blank" rel="noreferrer" href="https://github.com/sabriyeuregen">
-            <FontAwesomeIcon icon={faGithub} ></FontAwesomeIcon>
-           </a>
-       </li>
-     </ul>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3px"
+          className="close-icon"
+        ></FontAwesomeIcon>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sabriyeuregen/"
+          >
+            <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/sabriyeuregen"
+          >
+            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3px"
+        className="hamburger-icon"
+      ></FontAwesomeIcon>
     </div>
   )
 }
